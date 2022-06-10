ef4dcd2b61cf23233a7cfeda46588ea400aa2049
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-10 23:50:46 +0530

    [Tushar Vidwans] Checking Anagram

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Anagramcheck
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the first word: ");
+            string str1 = Console.ReadLine();
+            Console.Write("Enter the second word: ");
+            string str2 = Console.ReadLine();
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if(val1 == val2)
+            {
+                Console.WriteLine(val1);
+                Console.WriteLine("Both strings are Anagram");
+            }
+
+        }
+    }
+}
